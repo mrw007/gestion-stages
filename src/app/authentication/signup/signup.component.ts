b1638d795542e7b388a6c0f85ef60d2c3c07624c
Wahib <mr.wahb@gmail.com>
2017-11-13 21:01:55 +0100

    First commit

---
+++ src/app/authentication/signup/signup.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
+import {FormsModule} from '@angular/forms';
+
+const password = new FormControl('', Validators.required);
+const confirmPassword = new FormControl('', CustomValidators.equalTo(password));
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+
+  public form: FormGroup;
+  statuses=[
+      {name:"Enseignant",value:'enseignant'},
+      {name:"Etudiant",value:'etudiant'},
+      {name:"Entreprise",value:'entreprise'}   
+  ]
+  status:string='-1';
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    this.form = this.fb.group( {
+      uname: [null , Validators.compose ( [ Validators.required ] )],
+      password: password,
+      confirmPassword: confirmPassword
+    } );
+  }
+
+  onSubmit() {
+    this.router.navigate( ['/'] );
+  }
+print()
+{
+
+  console.log("Status",this.status);
+}
+}
---
+++ src/app/shared/menu-items/menu-items.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+
+export interface BadgeItem {
+  type: string;
+  value: string;
+}
+
+export interface ChildrenItems {
+  state: string;
+  name: string;
+  type?: string;
+}
+
+export interface Menu {
+  state: string;
+  name: string;
+  type: string;
+  icon: string;
+  badge?: BadgeItem[];
+  children?: ChildrenItems[];
+}
+
+const MENUITEMS = [
+  {
+    state: '/',
+    name: 'Acceuil',
+    type: 'link',
+    icon: 'basic-home'
+  },
+  {
+    state: 'offres_de_stages',
+    name: 'Offres de Stages',
+    type: 'link',
+    icon: 'basic-todo-pen'
+  },
+  {
+    state: 'offres_de_PFE',
+    name: 'Offres de PFE',
+    type: 'link',
+    icon: 'basic-todolist-pencil'
+  },
+  {
+    state: 'contact',
+    name: 'Contact',
+    type: 'link',
+    icon: 'basic-message-txt'
+  },
+  {
+    state: 'bibliotheque',
+    name: 'Bibliotheque',
+    type: 'link',
+    icon: 'basic-postcard'
+  },
+];
+
+@Injectable()
+export class MenuItems {
+  getAll(): Menu[] {
+    return MENUITEMS;
+  }
+}
