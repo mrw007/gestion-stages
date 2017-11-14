5fedbd9cd220db1227a1a0ef8230cb318cf68810
mrw007 <mr.wahib@gmail.com>
2017-11-14 12:03:47 +0100

    signin

--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -9,17 +10,32 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 })
 export class SigninComponent implements OnInit {
 
-  public form: FormGroup;
-  constructor(private fb: FormBuilder, private router: Router) {}
+  public formSignin: FormGroup;
 
+  //init General Form Values
+  post:any;  
+  username: string='';
+  password: string='';
+
+  constructor(private fb: FormBuilder, private router: Router) {
+
+  this.formSignin = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
+}
   ngOnInit() {
-    this.form = this.fb.group ( {
-      uname: [null , Validators.compose ( [ Validators.required ] )] , password: [null , Validators.compose ( [ Validators.required ] )]
-    } );
+   
   }
 
   onSubmit() {
     this.router.navigate ( [ '/' ] );
+  }
+  onSubmitSignin(post){
+    this.username = post.username;
+    this.password = post.password;
+    console.log("login", post);
+    
   }
 
 }
