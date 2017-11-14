8cfad566fdd3bc405c8f3ece0e9a8a3a110e8418
Wahib <mr.wahb@gmail.com>
2017-11-14 11:21:41 +0100

    signup form v1

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -14,7 +14,39 @@ const confirmPassword = new FormControl('', CustomValidators.equalTo(password));
 })
 export class SignupComponent implements OnInit {
 
-  public form: FormGroup;
+// init formGroups
+
+  public EtudiantForm: FormGroup;
+  public EnseignantForm: FormGroup;
+  public EntrepriseForm: FormGroup;
+
+//init General Form Values
+  post:any;  
+  nom: string='';
+  prenom: string='';
+  cin: string='';
+  email: string='';
+  pass:string='';
+  comf_pass: string='';
+
+//init Etudiant Form Values
+
+  dateNess: Date;
+  numInscrit: string='';
+  cy_etud: string='';
+  niv_etud: string='';
+
+//init Enseignant Form Values
+
+grade: string='';
+
+//init Entreprise Form Values
+
+nomEntreprise: string='';
+telEntreprise: string='';
+adresseEntreprise: string='';
+
+
   statuses=[
       {name:"Enseignant",value:'enseignant'},
       {name:"Etudiant",value:'etudiant'},
@@ -22,14 +54,49 @@ export class SignupComponent implements OnInit {
   ]
   status:string='-1';
   test:string;
-  constructor(private fb: FormBuilder, private router: Router) {}
+
+  
+  constructor(private fb: FormBuilder, private router: Router) {
+
+    this.EtudiantForm = new FormGroup({
+      nom: new FormControl(),
+      prenom: new FormControl(),
+      cin: new FormControl(),
+      email: new FormControl(),
+      pass:new FormControl(),
+      comf_pass: new FormControl(),
+      dateNess:  new FormControl(),
+      numInscrit:  new FormControl(),
+      cy_etud:  new FormControl(),
+      niv_etud:  new FormControl(),
+    });
+
+   this.EnseignantForm = new FormGroup({
+    nom: new FormControl(),
+    prenom: new FormControl(),
+    cin: new FormControl(),
+    email: new FormControl(),
+    pass:new FormControl(),
+    comf_pass: new FormControl(),
+    grade: new FormControl()
+    });
+
+    this.EntrepriseForm = new FormGroup({
+      nom: new FormControl(),
+      prenom: new FormControl(),
+      cin: new FormControl(),
+      email: new FormControl(),
+      pass:new FormControl(),
+      comf_pass: new FormControl(),
+      nomEntreprise: new FormControl(),
+      telEntreprise: new FormControl(),
+      adresseEntreprise: new FormControl()
+    });
+
+    }
 
   ngOnInit() {
-    this.form = this.fb.group( {
-      uname: [null , Validators.compose ( [ Validators.required ] )],
-      password: password,
-      confirmPassword: confirmPassword
-    } );
+
   }
 
   onSubmit() {
@@ -40,4 +107,55 @@ print()
 
   console.log("Status",this.status);
 }
+
+submitEtudiant(post)
+  {
+    this.nom = post.nom;
+    this.prenom = post.prenom;
+    this.cin = post.cin;
+    this.email = post.email;
+    this.pass =post.pass;
+    this.comf_pass = post.comf_pass;
+    this.dateNess = post.dateNess;
+    this.numInscrit = post.numInscrit;
+    this.cy_etud = post.cy_etud;
+    this.niv_etud = post.niv_etud;
+
+   //testing forms do not delete! 
+
+   console.log("submitEtudiant", post);  
+
+  }
+submitEntreprise(post)
+  {
+
+    this.nom = post.nom;
+    this.prenom = post.prenom;
+    this.cin = post.cin;
+    this.email = post.email;
+    this.pass =post.pass;
+    this.comf_pass = post.comf_pass;
+    this.nomEntreprise = post.nomEntreprise;
+    this.telEntreprise = post.telEntreprsie;
+    this.adresseEntreprise = post.adresseEntreprise;
+
+  //testing forms do not delete!
+  console.log("submitEntreprise", post);  
+
+  }  
+submitEnseignant(post)
+  {
+
+    this.nom = post.nom;
+    this.prenom = post.prenom;
+    this.cin = post.cin;
+    this.email = post.email;
+    this.pass =post.pass;
+    this.comf_pass = post.comf_pass;
+    this.grade = post.grade;
+
+  //testing forms do not delete!
+
+    console.log("submitEnseignant", post);
+  }  
 }
