9fd95602d02bda0736aff568459bb5887e3713a0
mrw007 <mr.wahib@gmail.com>
2017-11-15 10:00:14 +0100

    15/11/2017 10:00

--- src/app/social/social.component.ts
+++ src/app/social/social.component.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
+import {FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-social',
   templateUrl: './social.component.html',
   styleUrls: ['./social.component.scss']
 })
 export class SocialComponent {
-  images: any[] = [];
-  num = 1;
-
-  constructor() {
-    for ( this.num; this.num <= 18; this.num += 1 ) {
-      this.images.push( this.num );
-    }
-  }
+  
+  public modif_pass: FormGroup;
+  
+  //init General Form Values
+    post:any;  
+    pass:string='';
+    comf_pass: string='';
+  
+  constructor(private fb: FormBuilder, private router: Router) {
+    
+        this.modif_pass = new FormGroup({
+          pass:new FormControl(),
+          comf_pass: new FormControl()
+        });
+      }
+      onModif_pass(post)
+      {
+        this.pass =post.pass;
+        this.comf_pass = post.comf_pass;
+      //test
+        console.log("modif pass", post);  
+      }
 }
--- src/app/social/social.module.ts
+++ src/app/social/social.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgbProgressbarModule, NgbTabsetModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SocialComponent } from './social.component';
 import { SocialRoutes } from './social.routing';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(SocialRoutes), NgbProgressbarModule, NgbTabsetModule],
+  imports: [CommonModule, RouterModule.forChild(SocialRoutes), NgbProgressbarModule, NgbTabsetModule,FormsModule,ReactiveFormsModule],
   declarations: [SocialComponent]
 })
 
